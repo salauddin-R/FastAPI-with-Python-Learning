7f7b019c04d9862da3da109bc01d913c8ee66869
SALAUDDIN <mdsalauddinrony29@gmail.com>
2026-08-09 20:17:24 +0600

    add more option

--- app/main.py
+++ app/main.py
@@ -1,7 +1,33 @@
 from fastapi import FastAPI
+import uvicorn
 
 app = FastAPI()
 
 @app.get("/")
 def home():
-    return {"message": "Welcome to the FastAPI application! Start Working with me"}
+    return {"message":
+             "Welcome to the FastAPI application! Start workign with me and get ultra level experiance"}
+
+
+@app.put("/put")
+def putData():
+    return{
+        "message":"This is Put Data"
+    }
+
+@app.patch("/patch")
+def patchData():
+    return{
+        "message":"Patch Data"
+    }
+@app.delete("/delete")
+def deleteDat():
+    return{
+        "message":"delete data"
+    }
+
+@app.get("/get")
+def readData():
+    return{
+        "message":"Read the message"
+    }
